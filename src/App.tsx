cfb00984c0a98f01a81ed1a7e4673303297403bf
milon27 <mdjahidulislammilon@gmail.com>
2025-08-30 13:18:07 +0600

    shadcn ui done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Button } from "./components/ui/button";
+import { Card, CardContent } from "./components/ui/card";
+import { Input } from "./components/ui/input";
+
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      <Card>
+        <CardContent className="space-y-4">
+          <h1 className="text-3xl font-bold underline">
+            {" "}
+            Hello world! from tailwind
+          </h1>
+          <Input placeholder="Input From Shadcn" />
+          <Button variant={"default"}>Btn From Shadcn</Button>
+          <Button variant={"outline"}>Btn From Shadcn</Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
